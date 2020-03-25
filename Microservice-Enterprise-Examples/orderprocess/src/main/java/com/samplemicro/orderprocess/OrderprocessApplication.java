ac5fbbe1e0d92524b380594f12a4dec6c0cffd71
atanu231 <atanu.baidya@gmail.com>
2020-03-25 13:27:04 +0530

    Initial commit test

--- Microservice-Enterprise-Examples/orderprocess/src/main/java/com/samplemicro/orderprocess/OrderprocessApplication.java
+++ Microservice-Enterprise-Examples/orderprocess/src/main/java/com/samplemicro/orderprocess/OrderprocessApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderprocessApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderprocessApplication.class, args);
+		SpringApplication.run(OrderprocessApplication.class, args); 
 	}
 
 }
